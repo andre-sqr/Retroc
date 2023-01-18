2280f5104a572cebc39fc1bb00162250885c3af8
andre-sqr <andrebsqr@gmail.com>
2023-01-18 15:51:51 +0000

    redirecionamento no login para a página da conta

--- js/login.js
+++ js/login.js
@@ -1,4 +1,7 @@
 function login() {
-    alert(`Login bem sucedido!`)
-    location.href = '../html/userAccount.html'
+    const email = document.getElementById('email_login').value
+    if (email != '') {
+        alert(`Login bem sucedido com o e-mail ${email}!`)
+        window.location.href = '../html/userAccount.html';
+    }
 }
