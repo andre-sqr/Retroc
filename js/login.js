f7361a854edef526b0ed50cf3284747d1cba4313
andre-sqr <andrebsqr@gmail.com>
2023-01-20 13:15:00 +0000

    consertando redirecionamento da página de login

--- js/login.js
+++ js/login.js
@@ -1,7 +1,3 @@
 function login() {
-    const email = document.getElementById('email_login').value
-    if (email != '') {
-        alert(`Login bem sucedido com o e-mail ${email}!`)
-        window.location.href = '../html/userAccount.html';
-    }
+    window.location.href = './userAccount.html';
 }
